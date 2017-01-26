7a2b31e78ae04dcb87e981afc41d5844ea1fde39
Greg <mrgregking@msn.com>
2017-01-26 19:08:09 +0000

    clean up ui style sheet

--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -2,7 +2,7 @@ class StylesController < ApplicationController
   before_action :require_user, except: [:show]
   def show
     @style = Style.find(params[:id])
-    @recipes = @style.recipes.paginate(page: params[:page], per_page: 4)
+    @recipes = @style.recipes.paginate(page: params[:page], per_page:1)
   end
 
 
